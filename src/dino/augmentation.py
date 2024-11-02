4d943745ef149bb8a2d86eb42fa46809d66e54e9
dobbersc <conrad.dobberstein@informatik.hu-berlin.de>
2024-11-02 22:47:25 +0100

    Rename `num_augmentations` -> `repeats`

--- src/dino/augmentation.py
+++ src/dino/augmentation.py
@@ -10,29 +10,26 @@ Transform: TypeAlias = Callable[[Tensor], Tensor]
 class Augmenter(nn.Module):
     """Utility class that applies a series of augmentation transforms to an input image."""
 
-    def __init__(self, transforms: Transform | Sequence[Transform], num_augmentations: int | Sequence[int] = 1) -> None:
+    def __init__(self, transforms: Transform | Sequence[Transform], repeats: int | Sequence[int] = 1) -> None:
         """Initializes an Augmenter.
 
         :param transforms:
-        :param num_augmentations:
+        :param repeats:
         """
         super().__init__()
 
         self.transforms = transforms if isinstance(transforms, Sequence) else (transforms,)
-        self.num_augmentations = (
-            num_augmentations
-            if isinstance(num_augmentations, Sequence)
-            else (num_augmentations,) * len(self.transforms)
-        )
+        self.repeats = repeats if isinstance(repeats, Sequence) else (repeats,) * len(self.transforms)
 
-        if len(self.transforms) != len(self.num_augmentations):
+        if len(self.transforms) != len(self.repeats):
+            # TODO: Better error message
             msg: str = (
-                f"The number of transforms ({len(self.transforms)}) must match the number of "
-                f"augmentations to apply ({len(self.num_augmentations)})."
+                f"Mismatch between number of transforms ({len(self.transforms)}) and "
+                f"number of their repeats ({len(self.repeats)})."
             )
             raise ValueError(msg)
 
-        if any(n < 1 for n in self.num_augmentations):
+        if any(n < 1 for n in self.repeats):
             msg = ""  # TODO: Add error message
             raise ValueError(msg)
 
@@ -45,17 +42,17 @@ class Augmenter(nn.Module):
         return torch.stack(
             [
                 transform(image)
-                for transform, n in zip(self.transforms, self.num_augmentations, strict=True)
-                for _ in range(n)
+                for transform, repeat in zip(self.transforms, self.repeats, strict=True)
+                for _ in range(repeat)
             ],
             dim=0,
         )
 
 
 class DefaultLocalAugmenter(Augmenter):
-    def __init__(self, num_augmentations: int = 8) -> None:
+    def __init__(self, repeats: int = 8) -> None:
         # TODO: Integrate default local transformations
-        super().__init__(transforms=lambda x: x, num_augmentations=num_augmentations)
+        super().__init__(transforms=lambda x: x, repeats=repeats)
 
 
 class DefaultGlobalAugmenter(Augmenter):
@@ -63,4 +60,4 @@ class DefaultGlobalAugmenter(Augmenter):
         # TODO: Integrate default global transformations
         first_global_transform = lambda x: x  # noqa: E731
         second_global_transform = lambda x: x  # noqa: E731
-        super().__init__(transforms=[first_global_transform, second_global_transform], num_augmentations=(1, 1))
+        super().__init__(transforms=[first_global_transform, second_global_transform])
