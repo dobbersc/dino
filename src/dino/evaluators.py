787612821eb0bbd9f259aec0fbc72d1fe9431b79
dobbersc <conrad.dobberstein@informatik.hu-berlin.de>
2024-12-25 13:13:05 +0100

    Let the device be configurable for evaluators

--- src/dino/evaluators.py
+++ src/dino/evaluators.py
@@ -4,14 +4,15 @@ from typing import Any, TypeAlias
 
 import numpy as np
 import torch
+from numpy.typing import NDArray
 from sklearn.metrics import accuracy_score  # type: ignore[import-untyped]
 from sklearn.neighbors import KNeighborsClassifier  # type: ignore[import-untyped]
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from dino.utils.torch import detect_device
+
+from dino.utils.torch import get_module_device
 
 logger = logging.getLogger(__name__)
-device = detect_device()
 
 
 class Evaluator(ABC):
@@ -27,23 +28,32 @@ class KNNEvaluator(Evaluator):
     def __init__(self, eval_loader: DataLoader[Batch], train_loader: DataLoader[Batch], model: torch.nn.Module) -> None:
         self.eval_loader = eval_loader
         self.train_loader = train_loader
-        self.model = model.to(device)
+        self.model = model
 
     @torch.no_grad()
-    def evaluate(self, k: int = 20) -> float:
+    def evaluate(self, k: int = 20, device: torch.device | None = None) -> float:
+        device = get_module_device(self.model) if device is None else device
+
+        self.model.to(device)
         self.model.eval()
-        train_features, train_targets = self._extract_features(self.train_loader)
-        eval_features, eval_targets = self._extract_features(self.eval_loader)
-        knn = KNeighborsClassifier(n_neighbors=k, weights="distance")
+
+        train_features, train_targets = self._extract_features(self.train_loader, device)
+        eval_features, eval_targets = self._extract_features(self.eval_loader, device)
+
         logger.info("Fitting KNN model ...")
+        knn = KNeighborsClassifier(n_neighbors=k, weights="distance")
         knn.fit(train_features, train_targets)
+
         logger.info("Predicting ...")
         predictions = knn.predict(eval_features)
+
         logger.info("Calculating accuracy ...")
         return float(accuracy_score(eval_targets, predictions, normalize=True))
 
-    def _extract_features(self, loader: DataLoader) -> tuple[list[np.ndarray], list[np.ndarray]]:
-        features, targets = ([], [])
+    def _extract_features(
+        self, loader: DataLoader[Batch], device: torch.device,
+    ) -> tuple[list[NDArray[Any]], list[NDArray[Any]]]:
+        features, targets = [], []
         logger.info("Extracting features ...")
         for images, targets_ in tqdm(loader):
             # Move images to the appropriate device
@@ -62,11 +72,15 @@ class KNNEvaluator(Evaluator):
 class LinearEvaluator(Evaluator):
     def __init__(self, eval_loader: DataLoader[Batch], model: torch.nn.Module) -> None:
         self.eval_loader = eval_loader
-        self.model = model.to(device)
+        self.model = model
 
     @torch.no_grad()
-    def evaluate(self, topk: tuple[int, ...] = (1,)) -> dict[str, float]:
+    def evaluate(self, topk: tuple[int, ...] = (1,), device: torch.device | None = None) -> dict[str, float]:
+        device = get_module_device(self.model) if device is None else device
+
+        self.model.to(device)
         self.model.eval()
+
         total_counts = np.zeros(len(topk))
         for images, targets in tqdm(self.eval_loader):
             images = images.to(device)
