b46743808f4bccac010d492d5584a4a7f9621f5f
dobbersc <conrad.dobberstein@informatik.hu-berlin.de>
2025-01-24 21:17:25 +0100

    Add number of local views experiments

--- scripts/experiments_milestone3.sh
+++ scripts/experiments_milestone3.sh
@@ -33,3 +33,49 @@ nohup train \
   evaluation_dataset_dir=/vol/tmp/dobbersc-pub/imagenet100 \
   local_augmenter_crop_scale="[0.35, 0.40]" \
   > logs/ms3/local-view-crop-scale-0.35-0.40.out 2> logs/ms3/local-view-crop-scale-0.35-0.40.err < /dev/null &
+
+# Number Local Views (1)
+nohup train \
+  experiment_tag=ms3 \
+  model_tag=number-local-views-1 \
+  dataset.data_dir=/vol/tmp/dobbersc-pub/imagenet100/train \
+  evaluation_dataset_dir=/vol/tmp/dobbersc-pub/imagenet100 \
+  local_augmenter_num_views=1 \
+  > logs/ms3/number-local-views-1.out 2> logs/ms3/number-local-views-1.err < /dev/null &
+
+# Number Local Views (2)
+nohup train \
+  experiment_tag=ms3 \
+  model_tag=number-local-views-2 \
+  dataset.data_dir=/vol/tmp/dobbersc-pub/imagenet100/train \
+  evaluation_dataset_dir=/vol/tmp/dobbersc-pub/imagenet100 \
+  local_augmenter_num_views=2 \
+  > logs/ms3/number-local-views-2.out 2> logs/ms3/number-local-views-2.err < /dev/null &
+
+# Number Local Views (4)
+nohup train \
+  experiment_tag=ms3 \
+  model_tag=number-local-views-4 \
+  dataset.data_dir=/vol/tmp/dobbersc-pub/imagenet100/train \
+  evaluation_dataset_dir=/vol/tmp/dobbersc-pub/imagenet100 \
+  local_augmenter_num_views=4 \
+  > logs/ms3/number-local-views-4.out 2> logs/ms3/number-local-views-4.err < /dev/null &
+
+# Number Local Views (8)
+# Covered by default parameter run
+nohup train \
+  experiment_tag=ms3 \
+  model_tag=number-local-views-8 \
+  dataset.data_dir=/vol/tmp/dobbersc-pub/imagenet100/train \
+  evaluation_dataset_dir=/vol/tmp/dobbersc-pub/imagenet100 \
+  local_augmenter_num_views=8 \
+  > logs/ms3/number-local-views-8.out 2> logs/ms3/number-local-views-8.err < /dev/null &
+
+# Number Local Views (16)
+nohup train \
+  experiment_tag=ms3 \
+  model_tag=number-local-views-16 \
+  dataset.data_dir=/vol/tmp/dobbersc-pub/imagenet100/train \
+  evaluation_dataset_dir=/vol/tmp/dobbersc-pub/imagenet100 \
+  local_augmenter_num_views=16 \
+  > logs/ms3/number-local-views-16.out 2> logs/ms3/number-local-views-16.err < /dev/null &
