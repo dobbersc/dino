e5b4471f03fa372c29d46b336fd276414b72f50e
dobbersc <conrad.dobberstein@informatik.hu-berlin.de>
2025-01-26 21:05:13 +0100

    - Improve logging - Fix mlflow logging identifiers (batch vs. epoch) - Switch between eval and train mode for model

--- src/dino/simclr.py
+++ src/dino/simclr.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from torch.cuda.amp import GradScaler, autocast
 from tqdm import tqdm
 
+from dino import LOG_SEPARATOR
 from dino.utils.logging import mlflow_log_metrics
 
 logger = logging.getLogger(__name__)
@@ -29,7 +30,6 @@ def accuracy(output, target, topk=(1,)):
 
 
 class SimCLR:
-    LOG_STEPS = 100
 
     def __init__(
         self,
@@ -82,7 +82,7 @@ class SimCLR:
         # select and combine multiple positives
         positives = similarity_matrix[labels.bool()].view(labels.shape[0], -1)
 
-        # select only the negatives the negatives
+        # select only the negatives
         negatives = similarity_matrix[~labels.bool()].view(similarity_matrix.shape[0], -1)
 
         logits = torch.cat([positives, negatives], dim=1)
@@ -94,11 +94,24 @@ class SimCLR:
     def train(self, train_loader):
         scaler = GradScaler(enabled=self.fp16_precision)
 
-        n_iter = 0
+        logger.info(LOG_SEPARATOR)
         logger.info("Start SimCLR training for %d epochs.", self.epochs)
+        logger.info(LOG_SEPARATOR)
+        logger.info(self.model)
 
-        for epoch_counter in range(self.epochs):
-            for images in tqdm(train_loader):
+        n_iter = 1  # Total step counter
+        for epoch_counter in tqdm(range(1, self.epochs + 1)):
+            logger.info(LOG_SEPARATOR)
+            logger.info("EPOCH %d; STEP %d", epoch_counter, n_iter)
+
+            self.model.train()  # Set model into training mode at the beginning of each epoch
+
+            epoch_loss: float = 0.0
+            epoch_top1_accuracy: float = 0.0
+            epoch_top5_accuracy: float = 0.0
+            batch_index: int = 0
+
+            for images in tqdm(train_loader, unit="batch", leave=False):
                 images = torch.cat(images, dim=0)
 
                 images = images.to(self.device)
@@ -115,39 +128,70 @@ class SimCLR:
                 scaler.step(self.optimizer)
                 scaler.update()
 
-                if n_iter % self.LOG_STEPS == 0:
-                    top1, top5 = accuracy(logits, labels, topk=(1, 5))
-                    logger.info("Step %d: Loss = %.10f", n_iter, loss)
-                    logger.info("Step %d: Top-1 Accuracy = %.2f%%", n_iter, top1[0])
-                    logger.info("Step %d: Top-5 Accuracy = %.2f%%", n_iter, top5[0])
-                    learning_rate = self.scheduler.get_lr()[0]
-                    logger.info("Step %d: Learning Rate = %.6f", n_iter, learning_rate)
+                # Log step results
+                top1, top5 = accuracy(logits, labels, topk=(1, 5))
+                learning_rate = self.optimizer.param_groups[0]["lr"]
+                mlflow_log_metrics(
+                    "train_batch",
+                    metrics={
+                        "epoch": epoch_counter,
+                        "loss": loss,
+                        "learning_rate": learning_rate,
+                        "top1_train_accuracy": top1.item() / 100,
+                        "top5_train_accuracy": top5.item() / 100,
+                    },
+                    step=n_iter,
+                )
+
+                epoch_loss += loss.item()
+                epoch_top1_accuracy += top1.item()
+                epoch_top5_accuracy += top5.item()
+
+                if batch_index % max(1, len(train_loader) // 10) == 0:
+                    msg: str = (
+                        f"batch {batch_index + 1}/{len(train_loader)}"
+                        f" - step {n_iter}"
+                        f" - loss {loss:.8}"
+                        f" - top1 train accuracy {top1.item():.2f}%"
+                        f" - top5 train accuracy {top5.item():.5f}%"
+                        f" - lr {learning_rate:.8f}"
+                    )
+                    logger.info(msg)
 
                 n_iter += 1
+                batch_index += 1
+
+            logger.info("EPOCH %d DONE (step %d)", epoch_counter, n_iter)
+
+            # Average running epoch metrics
+            epoch_loss /= len(train_loader)
+            epoch_top1_accuracy /= len(train_loader)
+            epoch_top5_accuracy /= len(train_loader)
 
             metrics = {
-                "loss": loss.item(),
-                "top1-accuracy": top1[0].item(),
-                "top5-accuracy": top5[0].item(),
-                "learning-rate": learning_rate,
+                "loss": epoch_loss,
+                "top1_train_accuracy": epoch_top1_accuracy / 100,
+                "top5_train_accuracy": epoch_top5_accuracy / 100,
             }
 
+            # Evaluate model at the end of the epoch
             if self.evaluator is not None:
+                logger.info(LOG_SEPARATOR)
+                logger.info("Evaluation Phase")
                 eval_results = self.evaluator(self.model)
-                metrics.update(eval_results)
-                logger.info("Step %d: Evaluation results: %s", n_iter, eval_results)
+            else:
+                eval_results = {}
 
-            mlflow_log_metrics(
-                "train_batch",
-                metrics=metrics,
-                step=epoch_counter,
-            )
+            logger.info(LOG_SEPARATOR)
+            logger.info("Loss: %.8f", epoch_loss)
+            logger.info("Top-1 Train Accuracy: %.2f%%", epoch_top1_accuracy)
+            logger.info("Top-5 Train Accuracy: %.2f%%", epoch_top5_accuracy)
+            if eval_results:
+                logger.info("Evaluation Results: %r", eval_results)
+
+            # Log epoch metrics
+            mlflow_log_metrics("train_epoch", metrics=metrics | eval_results, step=epoch_counter)
+
             # warmup for the first 10 epochs
             if epoch_counter >= 10:
                 self.scheduler.step()
-            logger.debug(
-                "Epoch: %d\tLoss: %.4f\tTop1 accuracy: %.2f%%",
-                epoch_counter,
-                loss,
-                top1[0],
-            )
